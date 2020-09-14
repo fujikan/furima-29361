5b87e89130c8f65357bbc84b34d826d51ecb2dd2
fujikan <69848595+fujikan@users.noreply.github.com>
2020-09-14 19:53:50 +0900

    Userモデルの単体テストコード実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname {'fuji太郎'}
+    email {Faker::Internet.free_email}
+    password {'gnm01xyz'}
+    password_confirmation {password}
+    family_name{'藤崎'}
+    first_name{'幹太'}
+    family_name_kana{'フジサキ'}
+    first_name_kana{'カンタ'}
+    birth_day{'1996-02-19'}
+  end
+end
