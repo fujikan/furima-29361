884a6e22fadf3050c8106200ff653fa450751704
fujikan <69848595+fujikan@users.noreply.github.com>
2020-09-22 21:55:19 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,6 +34,14 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    if @item.destroy
+    redirect_to root_path
+  else
+    render :show
+  end
+end
   private
 
   def item_params
